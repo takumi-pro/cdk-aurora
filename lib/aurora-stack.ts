948c1d2224e110541478d2633d0bb1315d1a1b40
takumi-pro <takumidiary.0927@gmail.com>
2023-08-25 00:31:30 +0900

    Initial commit

--- lib/aurora-stack.ts
+++ lib/aurora-stack.ts
@@ -1,16 +1,52 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import * as rds from 'aws-cdk-lib/aws-rds'
 
 export class AuroraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC and Subnet
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+      availabilityZones: ['ap-northeast-1a', 'ap-northeast-1c'],
+      natGatewayProvider: ec2.NatProvider.instance({
+        instanceType: new ec2.InstanceType('t2.nano')
+      }),
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'PublicSubnet',
+          subnetType: ec2.SubnetType.PUBLIC
+        },
+        {
+          cidrMask: 24,
+          name: 'PrivateSubnet',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED
+        }
+      ]
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AuroraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const subnetGroup = new rds.SubnetGroup(this, 'SubnetGroup', {
+      vpc,
+      description: "RdsSubnetGroup",
+      subnetGroupName: 'RdsSubnetGroup',
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+      }
+    })
+
+    // aurora
+    new rds.DatabaseCluster(this, 'Database', {
+      engine: rds.DatabaseClusterEngine.auroraPostgres({
+        version: rds.AuroraPostgresEngineVersion.VER_14_7
+      }),
+      vpc,
+      writer: rds.ClusterInstance.serverlessV2('writer'),
+      serverlessV2MaxCapacity: 1,
+      serverlessV2MinCapacity: 0.5,
+      subnetGroup
+    })
   }
 }
